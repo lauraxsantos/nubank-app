c409e3f3b493122e14a513317a9164e8dd0bab86
Ana Laura <analaurass45@gmail.com>
2023-08-08 15:04:55 -0300

    feat: adding main component to App.js

---
+++ App.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native';
+import Main from './src/components/main/main';
+
+export default function App() {
+  return (
+      <View>
+        <Main></Main>
+      </View>
+  );
+}
+
+const styles = StyleSheet.create({
+
+
+});
